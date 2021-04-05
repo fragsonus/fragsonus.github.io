f861e43a5bf7c178ceb7ed581566cb723da3ab9d
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-05 14:44:31 +0800

    Update ethers_helper.js

--- js/ethers_helper.js
+++ js/ethers_helper.js
@@ -1288,12 +1288,12 @@ function getUniPrices(tokens, prices, pool)
         //   _print(`${t0.symbol} Price: $${formatMoney(p0)}`)
         // }
         // _print(`${t1.symbol} Price: $${formatMoney(p1)}`)
-        _print(`<h3>Prices</h3>`); 
+        _print(`<b>Prices</b>`); 
         _print(`${t0.symbol}: $${formatMoney(p0)} || LP: $${formatMoney(price)}`) 
         // _print(`${t0.symbol}: $${formatMoney(p0)} || ${t1.symbol}: $${formatMoney(p1)} || LP: $${formatMoney(price)}`)
         // _print(`Total Staked LP: ${formatMoney0(pool.staked)}`);
         // _print(`Total Staked USD: ${formatMoney0(staked_tvl)}`);
-        _print(`<h3>Total Staked</h3>`); 
+        _print(`<b>Total Staked</b>`); 
         _print(`USD: $${formatMoney0(staked_tvl)} || LP: ${formatMoney0(pool.staked)}`);
       },
       print_contained_price(userStaked) {
