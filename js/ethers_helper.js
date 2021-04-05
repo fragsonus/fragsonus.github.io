567cc28a0d6b3ebd4d1fe37cdddb66853533a25d
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-05 13:36:33 +0800

    Update ethers_helper.js

--- js/ethers_helper.js
+++ js/ethers_helper.js
@@ -1277,7 +1277,8 @@ function getUniPrices(tokens, prices, pool)
           `https://app.uniswap.org/#/swap?inputCurrency=${t0address}&outputCurrency=${t1address}` ]
         const helperHrefs = helperUrls.length == 0 ? "" :
           ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Swap]</a>`
-        _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
+        // _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
+        _print(`<h1>${stakeTokenTicker}</h1>`);
         _print(`${helperHrefs}`);
         _print(`LP Price: $${formatMoney(price)} TVL: $${formatMoney0(tvl)}`);
         if(p0 < 0.01){
@@ -1341,7 +1342,8 @@ function getValuePrices(tokens, prices, pool)
         ]
         const helperHrefs = helperUrls.length == 0 ? "" :
           ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Swap]</a>`
-        _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
+        // _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
+        _print(`<h1>${stakeTokenTicker}</h1>`);
         _print(`${helperHrefs}`);
         _print(`LP Price: $${formatMoney(price)} TVL: $${formatMoney0(tvl)}`);
         _print(`${t0.symbol} Price: $${formatMoney(p0)}`)
