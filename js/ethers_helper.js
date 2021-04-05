3c3a197c0dad3d30ad604d72c0a204906a128bba
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-05 15:39:24 +0800

    Update ethers_helper.js

--- js/ethers_helper.js
+++ js/ethers_helper.js
@@ -1601,7 +1601,7 @@ function printAPR(rewardTokenTicker, rewardPrice, poolRewardsPerWeek,
   var weeklyAPR = usdPerWeek / staked_tvl * 100;
   var dailyAPR = weeklyAPR / 7;
   var yearlyAPR = weeklyAPR * 52;
-  _print(`- APR -`); 
+  _print(`<u>APR</u>`); 
   _print(`Day: ${dailyAPR.toFixed(1)}% || Year: ${yearlyAPR.toFixed(0)}%`);
   _print(``);
   var userStakedUsd = userStaked * poolTokenPrice;
