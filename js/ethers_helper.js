c34271070f76ba590a1f6d4bd5de9c027b38639b
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-10 15:47:44 +0800

    Update ethers_helper.js

--- js/ethers_helper.js
+++ js/ethers_helper.js
@@ -1276,7 +1276,7 @@ function getUniPrices(tokens, prices, pool)
           `https://app.uniswap.org/#/remove/${t0address}/${t1address}`,
           `https://app.uniswap.org/#/swap?inputCurrency=${t0address}&outputCurrency=${t1address}` ]
         const helperHrefs = helperUrls.length == 0 ? "" :
-          ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Swap]</a>`
+          ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Buy]</a>`
         // _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
         _print(`<h2>${stakeTokenTicker}</h2>`);
         _print(`${helperHrefs}`);
@@ -1349,7 +1349,7 @@ function getValuePrices(tokens, prices, pool)
           `https://bsc.valuedefi.io/#/vswap?inputCurrency=${t0address}&outputCurrency=${t1address}` 
         ]
         const helperHrefs = helperUrls.length == 0 ? "" :
-          ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Swap]</a>`
+          ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Buy]</a>`
         // _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
         _print(`<h2>${stakeTokenTicker}</h2>`);
         _print(`${helperHrefs}`);
