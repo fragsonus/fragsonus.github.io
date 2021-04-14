b4804ce30b513e83d45f87b5c5d4f3543863c361
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-15 07:58:01 +0800

    links

--- Remake/js/ethers_helper.js
+++ Remake/js/ethers_helper.js
@@ -1297,12 +1297,17 @@ function getUniPrices(tokens, prices, pool)
         [ `https://app.uniswap.org/#/add/${t0address}/${t1address}`,
           `https://app.uniswap.org/#/remove/${t0address}/${t1address}`,
           `https://app.uniswap.org/#/swap?inputCurrency=${t0address}&outputCurrency=${t1address}` ]
-        const helperHrefs = helperUrls.length == 0 ? "" :
-          ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Buy]</a>`
+        // const helperHrefs = helperUrls.length == 0 ? "" :
+        //   ` <a href='${helperUrls[0]}' target='_blank'>[Add LP]</a> <a href='${helperUrls[1]}' target='_blank'>[Rmv LP]</a> <a href='${helperUrls[2]}' target='_blank'>[Buy]</a>`
         // _print(`<a href='${poolUrl}' target='_blank'>${stakeTokenTicker}</a>`);
-        _print(`<h2>${stakeTokenTicker}</h2>`);
+        // _print(`<h2>${stakeTokenTicker}</h2>`);
         // _print(`${helperHrefs}`);
         // _print(``);
+
+        var x = document.createElement("BUTTON");
+        var t = document.createTextNode(`${stakeTokenTicker}`);
+        x.appendChild(t);
+        logger.appendChild(x);
 
         var x = document.createElement("BUTTON");
         var t = document.createTextNode(`Add LP`);
