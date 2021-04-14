9ba0d3b0dd48879e82b9b6520868dd2b73dbeddb
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-14 22:53:46 +0800

    Update bsc_helper.js

--- Remake/js/bsc_helper.js
+++ Remake/js/bsc_helper.js
@@ -458,7 +458,8 @@ async function loadBscChefContract(App, tokens, prices, chef, chefAddress, chefA
   let aprs = []
   for (i = 0; i < poolCount; i++) {
     if (poolPrices[i]) {
-      logger = document.getElementById('log1')
+      x = 'log' + i ;
+      logger = document.getElementById(x)
       const apr = printChefPool(App, chefAbi, chefAddress, prices, tokens, poolInfos[i], i, poolPrices[i],
         totalAllocPoints, rewardsPerWeek, rewardTokenTicker, rewardTokenAddress,
         pendingRewardsFunction, null, null, "bsc")
