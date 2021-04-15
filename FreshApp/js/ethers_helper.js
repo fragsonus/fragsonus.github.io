fad9a10437100cb105d2343d4d65ccefad0665e5
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-15 22:35:43 +0800

    Update ethers_helper.js

--- FreshApp/js/ethers_helper.js
+++ FreshApp/js/ethers_helper.js
@@ -1241,6 +1241,22 @@ function formatMoney(amount, decimalCount = 2, decimal = ".", thousands = ",") {
   }
 }
 
+function formatMoney0(amount, decimalCount = 0, decimal = ".", thousands = ",") {
+  try {
+    decimalCount = Math.abs(decimalCount);
+    decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+    const negativeSign = amount < 0 ? "-" : "";
+
+    let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+    let j = (i.length > 3) ? i.length % 3 : 0;
+
+    return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+  } catch (e) {
+    console.log(e)
+  }
+}
+
 function getUniPrices(tokens, prices, pool)
 {
   var t0 = getParameterCaseInsensitive(tokens,pool.token0);
@@ -1408,7 +1424,7 @@ function getUniPrices(tokens, prices, pool)
 
         var x = document.createElement("P");
         x.setAttribute('class', 'alignright');
-        x.innerHTML = `$${formatMoney(staked_tvl)}`;
+        x.innerHTML = `$${formatMoney0(staked_tvl)}`;
         y.appendChild(x);
 
        // **********************************************************
@@ -1673,7 +1689,7 @@ function printAPR(rewardTokenTicker, rewardPrice, poolRewardsPerWeek,
 
   var x = document.createElement("P");
   x.setAttribute('class', 'alignright');
-  x.innerHTML = `$${formatMoney(userStakedUsd)}`;
+  x.innerHTML = `$${formatMoney0(userStakedUsd)}`;
   y.appendChild(x);
 
   var userWeeklyRewards = userStakedPct * poolRewardsPerWeek / 100;
