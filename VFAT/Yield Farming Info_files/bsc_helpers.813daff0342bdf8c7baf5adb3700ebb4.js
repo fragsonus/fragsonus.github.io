1da4789fabd322602e1b9e0cccb341c3fc836f5b
fragsonus <81014117+fragsonus@users.noreply.github.com>
2021-04-20 12:16:50 +0800

    Update bsc_helpers.813daff0342bdf8c7baf5adb3700ebb4.js

--- VFAT/Yield Farming Info_files/bsc_helpers.813daff0342bdf8c7baf5adb3700ebb4.js
+++ VFAT/Yield Farming Info_files/bsc_helpers.813daff0342bdf8c7baf5adb3700ebb4.js
@@ -456,6 +456,8 @@ async function loadBscChefContract(App, tokens, prices, chef, chefAddress, chefA
       poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "bsc") : undefined);
   }
 
+  _print(poolInfo.poolToken);
+
   const poolPrices = poolInfos.map(poolInfo => poolInfo.poolToken ? getPoolPrices(tokens, prices, poolInfo.poolToken, "bsc") : undefined);
 
   _print(poolPrices);
